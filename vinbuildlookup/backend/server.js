96e471faa3722d19bdf3d51e5ee6fdd3a7d0359d
forman025 <forman@ymail.com>
2026-03-23 01:08:30 -0400

    Update server.js

--- vinbuildlookup/backend/server.js
+++ vinbuildlookup/backend/server.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
